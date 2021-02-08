d1904114a51ae1336c53be743b3c5f143514d92a
Denis Bogolepov <bogolepovden@mail.ru>
2021-02-08 15:37:38 +0700

    Added change in 3.py
    Added empty function drow eyes
    Added function draw mouth

--- Python/lectures on Python/4 lab/3.py
+++ Python/lectures on Python/4 lab/3.py
@@ -5,11 +5,13 @@ from pygame.draw import rect, line, circle
 
 FPS = 30
 
+BACKGROUND = (242, 255, 255)
 YELLOW = (255, 211, 0)
 RED = (255, 51, 51)
-BLACK = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 screen = pygame.display.set_mode((600, 600))
+screen.fill(BACKGROUND)
 
 
 def main():
@@ -17,8 +19,9 @@ def main():
     pygame.init()
 
     size = 200
-    x = 300
-    y = 300
+    # Point in center screen
+    x = screen.get_width() // 2
+    y = screen.get_height() // 2
 
     draw_smiley(x, y, size)
 
@@ -40,12 +43,38 @@ def draw_smiley(x, y, size):
 
     Location smile x, y in senter with diameter size.
     """
-    draw_body(x, y, size)
+    size_body = size // 2
+    draw_body(x, y, size_body)
+
+    y_eyes = y - size // 8
+    draw_eyes(x, y_eyes, size)
+
+    y_mouth = y + 1/5 * size
+    size_mouth_w = 3/4 * size
+    size_mouth_h = 1/8 * size
+    draw_mouth(x, y_mouth, size_mouth_w, size_mouth_h)
 
 
 def draw_body(x, y, size):
     """Draw body smiley."""
     circle(screen, YELLOW, (x, y), size)
+
+
+def draw_eyes(x, y, size):
+    """Draw eyes and eyebrows smiley.
+
+    x, y show on point between eyes.
+    """
+
+
+def draw_mouth(x, y, width, height):
+    """Draw mouth.
+
+    x, y show center mouth.
+    width, height show size mouth.
+    """
+    rect(screen, BLACK, (x, y, width//3, height))
+    rect(screen, BLACK, (x-width//3, y, width//3, height))
 
 
 if __name__ == '__main__':
