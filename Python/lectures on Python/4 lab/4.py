b764d3dc7627f4ed2145c48a0fb359bb9edd8800
Denis Bogolepov <bogolepovden@mail.ru>
2021-02-09 21:09:37 +0700

    Realize function draw_body_ship()

--- Python/lectures on Python/4 lab/4.py
+++ Python/lectures on Python/4 lab/4.py
@@ -1,6 +1,6 @@
 """Draw picture beach."""
 import pygame
-from pygame.draw import rect, circle
+from pygame.draw import rect, circle, polygon
 from random import randint
 
 FPS = 30
@@ -9,6 +9,9 @@ DEEP_BLUE = (0, 191, 255)
 YELLOW = (255, 215, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+LIGHT_BROWN = (166, 64, 0)
+BROWN = (65, 25, 0)
+GREY = (81, 199, 209)  # 208
 
 
 screen = pygame.display.set_mode((600, 400))
@@ -34,10 +37,10 @@ def main():
     size_sun = 30
     draw_sun(x_sun, y_sun, size_sun)
 
-    x_ship = width // 4
-    y_ship = height // 8
-    width_ship = width // 3
-    height_ship = height // 4
+    x_ship = width // 1.5
+    y_ship = height // 2
+    width_ship = 250
+    height_ship = width_ship // 1.75
     draw_ship(x_ship, y_ship, width_ship, height_ship)
 
     x_umbrella = width // 4
@@ -95,7 +98,59 @@ def draw_sun(x, y, size):
 
 
 def draw_ship(x, y, width, height):
-    """Draw ship."""
+    """Draw ship.
+
+    x, y show position center up body ship
+    """
+    height_body_ship = height // 5
+    draw_body_ship(x, y, width, height_body_ship)
+
+    width_mast_ship = width // 3
+    height_mast_ship = height - height_body_ship
+    draw_mast_ship(x, y, width_mast_ship, height_mast_ship)
+
+
+def draw_body_ship(x, y, width, height):
+    """Draw body ship.
+
+    x, y show position center up body ship
+    """
+    # Draw end ship
+    radius_end = height
+    x_end = x - width // 2 + radius_end
+    circle(screen, LIGHT_BROWN, (x_end, y), radius_end, draw_bottom_left=True)
+    circle(screen, BROWN, (x_end, y), radius_end, 1, draw_bottom_left=True)
+
+    # Draw center ship
+    width_center = (width - radius_end) // 1.25
+    rect(screen, LIGHT_BROWN, (x_end, y, width_center, height))
+    rect(screen, BROWN, (x_end, y, width_center, height), 1)
+
+    # Draw start ship
+    x_start = x_end + width_center
+    y_start = y + height
+    x_width_start = x_start + width - width_center - radius_end
+    polygon(screen, LIGHT_BROWN, ([x_start, y], [x_width_start, y],
+                                  [x_start, y_start]))
+    polygon(screen, BROWN, ([x_start, y], [x_width_start, y],
+                            [x_start, y_start]), 1)
+    # Draw circle in start body
+    radius_start = height // 4
+    indent = radius_start // 2
+    x_start_circle = x_start + indent + radius_start
+    y_start_circle = y + indent + radius_start
+    circle(screen, WHITE, (x_start_circle, y_start_circle), radius_start)
+    indent_inside = int(radius_start // 3)
+    print(indent_inside)
+    circle(screen, BLACK, (x_start_circle, y_start_circle),
+           radius_start, indent_inside)
+
+
+def draw_mast_ship(x, y, width, height):
+    """Draw mast ship.
+
+    x, y show down point mast
+    """
     pass
 
 
